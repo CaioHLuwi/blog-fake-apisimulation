ba66051f964d3694f8d1d122f30cc927f3d8d53d
CaioHLuwi <caiohenriquelw2004@gmail.com>
2024-11-24 16:36:45 -0300

    add post requistion in form's inputs to add a new fictional posts a blog and logic

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,4 @@
 // https://jsonplaceholder.typicode.com/posts
-
-
 async function readPosts() {
     const spanElement = document.createElement('span');
     let postArea = document.querySelector('.posts');
@@ -8,13 +6,12 @@ async function readPosts() {
     let response = await fetch('https://jsonplaceholder.typicode.com/posts');
     let data = await response.json(); // JSON recebido e convertido para um objeto JS
     let loadingPosts = spanElement.textContent = 'Carregando...'; // Texto fictício enquanto não é encontrado nenhuma informação na API.
-
     postArea.append(loadingPosts);
 
     if(data.length > 0 ){
         postArea.innerHTML = ''
 
-        for(i of data){
+        for(let post in data){
             //Adiciona todos os elementos necessários para criação de um post
             const divElement = document.createElement('div');
             const h2Element = document.createElement('h2');
@@ -22,8 +19,8 @@ async function readPosts() {
             const hrElement = document.createElement('hr');
 
             //Insere o titulo e corpo do post dentro dos elementos criados anteriormente
-            h2Element.textContent = `${data[i].title}`
-            paragraphElement.textContent = `${data[i].body}`
+            h2Element.textContent = `${data[post].title}`
+            paragraphElement.textContent = `${data[post].body}`
             
             // Acrescenta todos os elementos dentro da div logs chamada postArea.
             postArea.append(divElement);
@@ -36,5 +33,46 @@ async function readPosts() {
         spanElement.innerText = 'Não foi encontrado nenhum post.'
     }
 }
+
+async function createPost(title, body) {
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts', 
+        { 
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title,
+                body,
+                userId: 2
+            })
+        }
+    );
+    let json = response.json();
+
+    console.log(json);    
+    // Não vai ser necessário fazer nada com a resposta gerada pelo fetch nesse projeto.
+
+    /* Aciona a função 'readPosts()' para recarregar os posts e dar a impressão de que um novo post 
+    foi adicionado */
+
+    document.querySelector('#titleField').value = '';
+    document.querySelector('#contentField').value = '';
+    
+    readPosts();
+}
+
+let insertButton = document.querySelector('#insertButton')
+
+insertButton.addEventListener('click', () => {
+    let titlePost = document.querySelector('#titleField').value;
+    let contentPost = document.querySelector('#contentField').value;
+
+    if(titlePost != '' && contentPost != ''){
+        createPost(titlePost, contentPost);
+    } else {
+        alert('Preencha os dois campos corretamente.');
+    }
+})
 
 readPosts();
